82661e845a9e77179184a974e94283d2ab579049
wilcrofter <bill@crofter.net>
2014-01-30 11:27:53 -0500

    quick and dirty template for small module creation.

--- R/qdTemplate.R
+++ R/qdTemplate.R
@@ -1,23 +1,23 @@
+modpath <- "inst/Courses/Test_Modules/module?/mod?_new.csv"
+if(!file.exists(dirname(modapth)))file.create(dirname(modpath), recursive=TRUE)
 
-```{r}
 newrow <- function(Class=NA, Output=NA, CorrectAnswer=NA, AnswerChoices=NA, AnswerTests=NA, Hint=NA, Figure=NA, FigureType=NA, VideoLink=NA){
   temp <- data.frame(Class=Class, Output=Output, CorrectAnswer=CorrectAnswer, 
                      AnswerChoices=AnswerChoices, AnswerTests=AnswerTests, Hint=Hint,
                      Figure=Figure, FigureType=FigureType, VideoLink=VideoLink)
 }
-module <- newrow(NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
-```
 
-text
-```{r}
+module <- newrow(NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+
+
+# text
 module <- rbind(module, newrow(
   Class="text", 
   Output="stuff"
   ))
-```
 
-mult_question
-```{r}
+
+# mult_question
 module <- rbind(module, newrow(
   Class="mult_question",
   Output="stuff",
@@ -25,10 +25,9 @@ module <- rbind(module, newrow(
   CorrectAnswer="ANS"
   AnswerTests="matches=ANS"
   ))
-```
 
-cmd_question
-```{r}
+
+# cmd_question
 module <- rbind(module, newrow(
   Class="cmd_question",
   Output="stuff",
@@ -36,30 +35,26 @@ module <- rbind(module, newrow(
   AnswerTests="uses_func=;equivalent=;creates_var=;equals=expr,name;identical=expr,val_length=",
   Hint="hint"
   ))
-```
 
-video              
-```{r}
+
+# video              
 module <- rbind(module, newrow(
   Class="video",
   Output="stuff",
   VideoLink="http://address.of.video"
   ))
-```
 
-figure
-```{r}
+
+# figure
 module <- rbind(module, newrow(
   Class="figure",
   Output="stuff",
   Figure="sourcefile.R",
   FigureType="new or old"
   ))
-```
 
 
-exact_question
-```{r}
+# exact_question
 module <- rbind(module, newrow(
   Class="exact_question",
   Output="stuff",
@@ -67,11 +62,10 @@ module <- rbind(module, newrow(
   AnswerTests="exact=n"
   Hint="hint"
   ))
-```
 
 
-range_question
-```{r}
+
+# range_question
 module <- rbind(module, newrow(
   Class="range_question",
   Output="stuff",
@@ -79,10 +73,9 @@ module <- rbind(module, newrow(
   AnswerTests="range=n1,n2"
   Hint="hint"
   ))
-```
 
-text_question
-```{r}
+
+# text_question
 module <- rbind(module, newrow(
   Class="text_question",
   Output="stuff",
@@ -90,10 +83,9 @@ module <- rbind(module, newrow(
   AnswerTests="matches=word"
   Hint="hint"
   ))
-```
 
-text_many_question
-```{r}
+
+# text_many_question
 module <- rbind(module, newrow(
   Class="text_many_question",
   Output="stuff",
@@ -101,11 +93,8 @@ module <- rbind(module, newrow(
   AnswerTests="word_many=word1,word2,word3",
   Hint="hint"
   ))
-```
 
-
-text_order_question       
-```{r}
+# text_order_question       
 module <- rbind(module, newrow(
   Class="text_order_question",
   Output="stuff",
@@ -113,5 +102,5 @@ module <- rbind(module, newrow(
   AnswerTests="word_order=word1,word2,word3",
   Hint="hint"
   ))
-```
 
+write.csv(module, file=modpath, row.names=FALSE)
